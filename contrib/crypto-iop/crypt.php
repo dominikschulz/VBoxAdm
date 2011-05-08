268098df57dbba3428930d595d16b71d90a61d18
Dominik Schulz <dominik.schulz@gauner.org>
2011-05-08 19:35:13 +0200

    Work on API.
    Work on RoleAccounts.
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- contrib/crypto-iop/crypt.php
+++ contrib/crypto-iop/crypt.php
@@ -1,18 +1,32 @@
 <?php
 
-$key = "password";
-for ($i = strlen($key); $i < 56; $i++) {
-  $key .= '0';
-}
-print "Key: $key - Len: ".strlen($key)."\n";
-$input = "cleartext";
-$iv = "1";
-for ($i = strlen($iv); $i < 8; $i++) {
-  $iv .= '1';
+/*
+ * this example needs the following php extensions:
+ * - mcrypt
+ * - mhash
+ * - json
+ * - curl
+ */
+
+$config['api']['key'] = 'key';
+
+$request['mailbox']['update']['user@domain123.com']['password'] = 'password123';
+$ciphertext = encrypt($config['api']['key'],$request);
+
+// TODO send request to API endpoint via curl
+
+function encrypt($raw_key,$request) {
+	return mcrypt_cbc(MCRYPT_BLOWFISH,gen_key($raw_key),json_encode($request),MCRYPT_ENCRYPT,gen_iv($raw_key));
 }
 
-$crypt = mcrypt_cbc(MCRYPT_BLOWFISH, $key, $input, MCRYPT_ENCRYPT, $iv);
-$crypt_b64 = base64_encode($crypt);
-print "Crypt: $crypt_b64\n";
+function decrypt($raw_key,$ciphertext) {
+	return json_decode(mcrypt_cbc(MCRYPT_BLOWFISH,gen_key($raw_key),$ciphertext,MCRYPT_DECRYPT,gen_iv($raw_key)));
+}
 
+function gen_key($raw_key) {
+	return substr(mhash(MHASH_SHA512,$raw_key),0,56);
+}
+function gen_iv($raw_key) {
+	return substr(mhash(MHASH_SHA512,$raw_key),56);
+}
 ?>
